9ef434bac7a8201ab7f8c412cb234816091b19af
WardaRashidCh <wardarashid650@gmail.com>
2025-12-18 11:24:20 -0800

    Add files via upload

---
+++ Task 3.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int num;
+    cout << "Enter a positive number: ";
+    cin >> num;
+    while (num <= 0) {
+        cout << "Invalid input. Enter a positive number: ";
+        cin >> num;
+    }
+    cout << "You entered: " << num;
+    return 0;
+}
